e1ddd0589b96804c98ed00ea9fe359522a622365
mykolamayorov <mykolamayorov@gmail.com>
2025-03-15 13:11:47 +0200

    initial

---
+++ task1/total_salary.py
@@ -0,0 +1,18 @@
+# Функція яка аналізує файл.txt і повертає загальну та середню суму заробітної плати всіх розробників.
+
+from pathlib import Path
+
+current_directory = Path.cwd()
+print(current_directory)
+
+script_directory = Path(__file__).parent
+print(script_directory)
+
+
+def total_salary(path):
+    relative_path = Path(path)
+    path = relative_path
+    print(path.read_text()) 
+    print("Exists:", path.exists()) 
+
+total_salary("task1/salary.txt")
